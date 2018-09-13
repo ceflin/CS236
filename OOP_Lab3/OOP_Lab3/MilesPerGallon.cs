9717f5b989a817f367abbc36b5156ab39eab3d49
ceflin <ceflin@stumail.jccc.edu>
2018-09-13 15:03:10 -0500

    lab 3

---
+++ OOP_Lab3/OOP_Lab3/MilesPerGallon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab3
+{
+    class MilesPerGallon
+    {
+        private double _gallons;
+        private double _miles;
+        private double _mpg;
+
+        public double Miles
+        {
+            get => _miles;
+            set
+            {
+                ValidateNonNegative(value);
+                ValidatNonZero(value);
+                _miles = value;
+            }
+        }
+
+
+
+        public double Gallons
+        {
+            get => _gallons;
+            set
+            {
+                ValidateNonNegative(value);
+                _gallons = value;
+            }
+        }
+
+        public double Mpg => Miles / Gallons;
+
+        private void ValidatNonZero(double value)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private void ValidateNonNegative(double value)
+        {
+            if (value < 0) throw NegativeNumberException();
+        }
+    }
+
+}
---
+++ OOP_Lab3/OOP_Lab3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var continueLoop = true;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter the miles: ");
+                    double.TryParse(Console.ReadLine(), out var miles);
+                    Console.WriteLine();
+                    Console.Write("Enter the gallons: ");
+                    double.TryParse(Console.ReadLine(), out var gallons);
+
+                    var result = miles / gallons;
+
+                    Console.WriteLine($"\nResult: {miles}/{gallons} = {result}");
+                    continueLoop = false;
+                }
+                catch (DivideByZeroException divideByZeroEx)
+                {
+                    Console.WriteLine($"{divideByZeroEx.Message}");
+                    Console.WriteLine("0 is an invalid number of gallons.");
+                }
+                catch (NegativeNumberException negativeNumberEx)
+                {
+                    Console.WriteLine($"{negativeNumberEx.Message}");
+                    
+                }
+            } while (continueLoop);
+        }
+    }
+}
